de4cb588b09d1552063c2c19447eb70a9c6c891d
Rian Gallagher <rianogallagher@gmail.com>
2019-07-10 20:53:31 +0100

    add handler for getting week's distance

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import(
   "fmt"
   "os"
+  "log"
+  "net/http"
+  "io/ioutil"
+  "encoding/json"
+  "time"
   "github.com/mikeflynn/go-alexa/skillserver"
 )
 
@@ -18,6 +23,12 @@ var (
 	}
 )
 
+type Activities []struct {
+  Name string `json:"name"`
+  Distance float32 `json:"distance"`
+  Date string `json:"start_date"`
+}
+
 func launchHandler(request *skillserver.EchoRequest, echoResponse *skillserver.EchoResponse) {
 	echoResponse.OutputSpeech("You have successfully launched a new session.")
 	echoResponse.EndSession(false)
@@ -28,7 +39,61 @@ func sessionEndedHandler(request *skillserver.EchoRequest, echoResponse *skillse
 }
 
 func intentHandler(request *skillserver.EchoRequest, echoResponse *skillserver.EchoResponse) {
-	echoResponse.OutputSpeech(fmt.Sprintf("You have invoked the %s intent.", request.GetIntentName()))
+  var response *skillserver.EchoResponse
+
+  switch request.GetIntentName() {
+  case "GetWeekDistance":
+    response = handleGetWeekDistanceIntent(request)
+  default:
+    echoResponse.OutputSpeech(fmt.Sprintf("You have invoked the %s intent.", request.GetIntentName()))
+  }
+
+  if response == nil {
+    response = skillserver.NewEchoResponse()
+    response.OutputSpeech("Sorry, something went wrong")
+  }
+
+  *echoResponse = *response
+}
+
+func handleGetWeekDistanceIntent(request *skillserver.EchoRequest) *skillserver.EchoResponse {
+  var bearer = "Bearer " + request.Session.User.AccessToken
+
+  url := "https://www.strava.com/api/v3/athlete/activities?after=1562554008"
+
+  req, err := http.NewRequest("GET", url, nil)
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  req.Header.Add("Authorization", bearer)
+
+  client := &http.Client{Timeout: 10 * time.Second}
+  res, err := client.Do(req)
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  body, err := ioutil.ReadAll(res.Body)
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  var responseObject Activities
+  json.Unmarshal(body, &responseObject)
+  log.Println(len(responseObject))
+
+  var totalDistance float32
+
+  for i := 0; i < len(responseObject); i++ {
+    log.Println(responseObject[i].Distance)
+    totalDistance += responseObject[i].Distance
+  }
+
+  response := skillserver.NewEchoResponse()
+  response.OutputSpeech(fmt.Sprintf("You have run %.2f kilometers this week.", totalDistance / 1000))
+
+  return response
 }
 
 func main() {
